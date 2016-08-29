25157c7ba51c9e14d2e680834b02302e560e44fc
Olli Raitio <olli.raitio@helsinki.fi>
2016-08-29 15:50:46 +0300

    support for noneditable data layers. GeoJSON layers styles can be customized. Support for disabling controls.

--- playground/app.js
+++ playground/app.js
@@ -5,25 +5,91 @@ import "../src/styles.js";
 class App {
 	constructor() {
 		this.data = [
-			{"type":"Feature","properties":{},"geometry":{"type":"Point","coordinates":[22.207004189222086,60.47430300256853]}},
-			{"type":"Feature","properties":{},"geometry":{"type":"Point","coordinates":[22.311658377997933,60.43453495634962]}},
 			{
-				"type": "Feature",
-				"properties": {},
-				"geometry": {
-					"type": "Point",
-					"coordinates": [
-						22.104264017028992,
-						60.40403173483798
-					],
-					"radius": 1955.2645542879416
+				data: [
+					{
+						"type": "Feature",
+						"properties": {},
+						"geometry": {
+							"type": "Point",
+							"coordinates": [
+								22.344264017028992,
+								60.40403173483798
+							],
+							"radius": 7000
+						}
+					},
+					{
+						"type": "Feature",
+						"properties": {},
+						"geometry": {
+							"type": "Point",
+							"coordinates": [
+								21.824264017028992,
+								60.40403173483798
+							],
+							"radius": 6000
+						}
+					}
+				]
+			}
+			,
+			{
+				data: [
+					{
+						"type": "Feature",
+						"properties": {},
+						"geometry": {
+							"type": "Point",
+							"coordinates": [
+								22.704264017028992,
+								60.40403173483798
+							],
+							"radius": 4000
+						}
+					}
+				],
+				getFeatureStyle: idx => {
+					return {
+						weight: idx,
+						opacity: 1,
+						fillOpacity: 1,
+						color: "#0f0"
+					}
 				}
 			}
 		]
+		this.drawData = {
+			data: [
+				{"type":"Feature","properties":{},"geometry":{"type":"Point","coordinates":[22.207004189222086,60.47430300256853]}},
+				{"type":"Feature","properties":{},"geometry":{"type":"Point","coordinates":[22.311658377997933,60.43453495634962]}},
+				{
+					"type": "Feature",
+					"properties": {},
+					"geometry": {
+						"type": "Point",
+						"coordinates": [
+							22.104264017028992,
+							60.40403173483798
+						],
+						"radius": 1955.2645542879416
+					}
+				}
+			],
+			getFeatureStyle: idx => {
+				return {
+					weight: idx,
+					opacity: 1,
+					fillOpacity: 0.4,
+					color: "#f00"
+				}
+			}
+		}
 		this.activeIdx = 0;
 
 		this.map = new LajiMap({
 			rootElem: document.getElementById("root"),
+			drawData: this.drawData,
 			data: this.data,
 			activeIdx: this.activeIdx,
 			latlng: [60.4353462, 22.2285623],
@@ -32,7 +98,6 @@ class App {
 			getPopup: this.getPopup,
 			lang: "fi"
 		});
-		console.log(this.map);
 
 		["fi", "en", "sv"].forEach(lang => {
 			document.getElementById(lang).addEventListener("click", () => this.map.setLang(lang))
@@ -41,24 +106,23 @@ class App {
 	}
 
 	getPopup = (idx, callback) => {
-		//return "" + idx;
-		callback("" + idx);
+		return "" + idx;
 	}
 
 	onMapChange = (events) => {
-		let { data } = this;
+		let { drawData } = this;
 		events.forEach(e => {
 			console.log(e);
 			switch (e.type) {
 				case "create":
-					data.push(e.data);
+					drawData.data.push(e.data);
 					break;
 				case "delete":
-					this.data = this.data.filter((item, i) => !e.idxs.includes(i));
+					this.drawData.data = this.drawData.data.filter((item, i) => !e.idxs.includes(i));
 					break;
 				case "edit":
 					for (let idx in e.data) {
-						this.data[idx] = e.data[idx];
+						this.drawData.data[idx] = e.data[idx];
 					}
 					break;
 				case "active":

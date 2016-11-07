b1af5730841de0550fa6ced29611ae43c6ab2d4b
Olli Raitio <olli.raitio@helsinki.fi>
2016-11-07 12:20:46 +0200

    update leaflet to 1.*

--- src/map.js
+++ src/map.js
@@ -4,7 +4,7 @@ import "proj4leaflet";
 import "leaflet-contextmenu";
 import "Leaflet.vector-markers";
 import "leaflet.markercluster";
-import "./lib/Leaflet.MML-layers/mmlLayers.js";
+import "leaflet-mml-layers";
 import "./lib/Leaflet.rrose/leaflet.rrose-src.js";
 import fetch from "isomorphic-fetch";
 import queryString from "querystring";

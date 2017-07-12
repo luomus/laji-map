luomus/laji-map

commit c7183a8e4d0c5505410c0e2f4912b1890d08c809

fix custom controls

playground/app.js
@@ -151,7 +151,7 @@ class App {
 			},
 			data: this.data,
 			tileLayerName: "openStreetMap",
-			overlayNames: ["ykjGrid", "ykjGridLabels"],
+			overlayNames: ["ykjGrid", "ykjGridLabels"]
 		};
 
 		const map = new LajiMap(options);

src/controls.js
@@ -422,7 +422,7 @@ export default LajiMap => class LajiMapWithControls extends LajiMap {
 			if (!splitted) {
 				const controlItem = controlSettings[controlName];
 				return (
-					controlItem &&
+					!controlItem || // Pass custom controls
 					dependenciesAreOk(controlName) &&
 					(controlItem.constructor !== Object || Object.keys(controlItem).some(name => controlItem[name]))
 				);

commit 91780fde8bcc30721f4af81984236ce2fb40f29a

fix bug introduced by c7183a8e4d0c5505410c0e2f4912b1890d08c809 (fix custom controls

playground/app.js
@@ -145,10 +145,7 @@ class App {
 			zoom: 11,
 			markerPopupOffset: 40,
 			featurePopupOffset: 5,
-			controlSettings: {
-				drawCopy: true,
-				drawUpload: true
-			},
+			controlSettings: false,
 			data: this.data,
 			tileLayerName: "openStreetMap",
 			overlayNames: ["ykjGrid", "ykjGridLabels"]

src/controls.js
@@ -422,7 +422,8 @@ export default LajiMap => class LajiMapWithControls extends LajiMap {
 			if (!splitted) {
 				const controlItem = controlSettings[controlName];
 				return (
-					!controlItem || // Pass custom controls
+					!(controlName in controlSettings) ||
+					controlItem && // Pass custom controls
 					dependenciesAreOk(controlName) &&
 					(controlItem.constructor !== Object || Object.keys(controlItem).some(name => controlItem[name]))
 				);
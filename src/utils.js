befd29fd44c297b1bebd7c36c06a9716449fb5d2
Olli Raitio <olli.raitio@helsinki.fi>
2017-10-20 14:24:23 +0300

    fix geojson-are dep & add circle-to-polygon dep

--- src/utils.js
+++ src/utils.js
@@ -5,6 +5,7 @@ import {
 	EPSG2393WKTString,
 	EPSG3067WKTString
 } from "./globals";
+import circleToPolygon from "circle-to-polygon";
 
 export function reverseCoordinate(c) {
 	return c.slice(0).reverse();
@@ -54,49 +55,6 @@ export function convertGeoJSON(geoJSON, from, to) {
 		if (key === "coordinates") obj = convertCoordinates(obj);
 		return obj;
 	});
-}
-
-/** Taken from https://github.com/arg20/circle-to-radius
- *  (Copied here because the library didn't act nice with exporting)
-**/
-function circleToPolygon(center, radius, numberOfSegments) {
-	function toRadians(angleInDegrees) {
-		return angleInDegrees * Math.PI / 180;
-	}
-
-	function toDegrees(angleInRadians) {
-		return angleInRadians * 180 / Math.PI;
-	}
-
-	function offset(c1, distance, bearing) {
-		var lat1 = toRadians(c1[1]);
-		var lon1 = toRadians(c1[0]);
-		var dByR = distance / 6378137; // distance divided by 6378137 (radius of the earth) wgs84
-		var lat = Math.asin(
-			Math.sin(lat1) * Math.cos(dByR) +
-			Math.cos(lat1) * Math.sin(dByR) * Math.cos(bearing));
-		var lon = lon1 + Math.atan2(
-				Math.sin(bearing) * Math.sin(dByR) * Math.cos(lat1),
-				Math.cos(dByR) - Math.sin(lat1) * Math.sin(lat));
-		return [toDegrees(lon), toDegrees(lat)];
-	}
-
-	var n = numberOfSegments ? numberOfSegments : 32;
-	var flatCoordinates = [];
-	var coordinates = [];
-	for (let i = 0; i < n; ++i) {
-		flatCoordinates.push.apply(flatCoordinates, offset(center, radius, 2 * Math.PI * i / n));
-	}
-	flatCoordinates.push(flatCoordinates[0], flatCoordinates[1]);
-
-	for (let i = 0, j = 0; j < flatCoordinates.length; j += 2) {
-		coordinates[i++] = flatCoordinates.slice(j, j + 2);
-	}
-
-	return {
-		type: "Polygon",
-		coordinates: [coordinates]
-	};
 }
 
 export function standardizeGeoJSON(geoJSON) {

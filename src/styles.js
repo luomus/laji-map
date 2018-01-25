8bcc96dd0dbc18d30d984e8cf3f72c958c23580c
Olli Raitio <olli.raitio@helsinki.fi>
2018-01-25 11:29:06 +0200

    update leaflet to 1.3.1

--- src/styles.js
+++ src/styles.js
@@ -1,5 +1,5 @@
 export * from "leaflet/dist/leaflet.css";
-export * from "leaflet-draw/dist/leaflet.draw.css";
+export * from "leaflet-draw/dist/Leaflet.draw.css";
 export * from "leaflet-contextmenu/dist/leaflet.contextmenu.css";
 export * from "Leaflet.vector-markers/dist/leaflet-vector-markers.css";
 export * from "leaflet.markercluster/dist/MarkerCluster.Default.css";

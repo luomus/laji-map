a06bc0c0da4fff8c7c34b2d4d07f7fc0d232a830
Olli Raitio <olli.raitio@helsinki.fi>
2018-09-05 12:09:12 +0300

    move contextmenu definitions to map

--- src/@types/leaflet-contextmenu/index.d.ts
+++ src/@types/leaflet-contextmenu/index.d.ts
@@ -1,48 +1,50 @@
-import * as L from "leaflet";
+// Doesn't work for some reason in compiled code. Included in src/map.ts.
 
-interface ContextmenuItemOptions {
-	text: string;
-	iconCls: string;
-	callback: () => void;
-}
+//import * as L from "leaflet";
+//
+//interface ContextmenuItemOptions {
+//	text: string;
+//	iconCls: string;
+//	callback: () => void;
+//}
+//
+//interface ContextmenuOptions {
+//	contextmenu?: boolean;
+//	contextmenuInheritItems?: boolean;
+//	contextmenuItems?: ContextmenuItemOptions[];
+//	contextmenuWidth?: number;
+//}
+//
+//export class ContextmenuItem {
+//}
+//
+//interface Contextmenu {
+//	addItem(options: ContextmenuItemOptions | "-"): HTMLElement;
+//	removeAllItems(): void;
+//	setDisabled(elem: HTMLElement | number, disabled: boolean): this;
+//	isVisible(): boolean;
+//}
+//
+//declare module "leaflet" {
+//	interface MapOptions extends ContextmenuOptions { }
+//
+//	interface Path {
+//		bindContextMenu(options: ContextmenuOptions): Path;
+//		unbindContextMenu();
+//	}
+//
+//	interface Marker {
+//		bindContextMenu(options: ContextmenuOptions): Marker;
+//		unbindContextMenu();
+//	}
+//
+//	interface Map {
+//		contextmenu: Contextmenu;
+//	}
 
-interface ContextmenuOptions {
-	contextmenu?: boolean;
-	contextmenuInheritItems?: boolean;
-	contextmenuItems?: ContextmenuItemOptions[];
-	contextmenuWidth?: number;
-}
-
-export class ContextmenuItem {
-}
-
-interface Contextmenu {
-	addItem(options: ContextmenuItemOptions | "-"): HTMLElement;
-	removeAllItems(): void;
-	setDisabled(elem: HTMLElement | number, disabled: boolean): this;
-	isVisible(): boolean;
-}
-
-declare module "leaflet" {
-	interface MapOptions extends ContextmenuOptions { }
-
-	interface Path {
-		bindContextMenu(options: ContextmenuOptions): Path;
-		unbindContextMenu();
-	}
-
-	interface Marker {
-		bindContextMenu(options: ContextmenuOptions): Marker;
-		unbindContextMenu();
-	}
-
-	interface Map {
-		contextmenu: Contextmenu;
-	}
-
-	namespace Contextmenu {
-		interface Options extends ContextmenuOptions {}
-		interface ItemOptions extends ContextmenuItemOptions {}
-	}
-}
+//	namespace Contextmenu {
+//		interface Options extends ContextmenuOptions {}
+//		interface ItemOptions extends ContextmenuItemOptions {}
+//	}
+//}
 
--- src/map.ts
+++ src/map.ts
@@ -56,6 +56,52 @@ L.Draw.Tooltip = L.Draw.Tooltip.extend({
 	}
 });
 
+interface ContextmenuItemOptions {
+	text: string;
+	iconCls: string;
+	callback: () => void;
+}
+
+interface ContextmenuOptions {
+	contextmenu?: boolean;
+	contextmenuInheritItems?: boolean;
+	contextmenuItems?: ContextmenuItemOptions[];
+	contextmenuWidth?: number;
+}
+
+export class ContextmenuItem {
+}
+
+interface Contextmenu {
+	addItem(options: ContextmenuItemOptions | "-"): HTMLElement;
+	removeAllItems(): void;
+	setDisabled(elem: HTMLElement | number, disabled: boolean): this;
+	isVisible(): boolean;
+}
+
+declare module "leaflet" {
+	interface MapOptions extends ContextmenuOptions { }
+
+	interface Path {
+		bindContextMenu(options: ContextmenuOptions): Path;
+		unbindContextMenu();
+	}
+
+	interface Marker {
+		bindContextMenu(options: ContextmenuOptions): Marker;
+		unbindContextMenu();
+	}
+
+	interface Map {
+		contextmenu: Contextmenu;
+	}
+
+	namespace Contextmenu {
+		interface Options extends ContextmenuOptions {}
+		interface ItemOptions extends ContextmenuItemOptions {}
+	}
+}
+
 export interface LajiMapFitBoundsOptions extends L.FitBoundsOptions {
 	paddingInMeters?: number;
 	minZoom?: number;

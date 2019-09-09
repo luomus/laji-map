7f518f9777034ff1d53a9161574019baf72743b7
Olli Raitio <olli.raitio@helsinki.fi>
2019-09-09 12:16:19 +0300

    fix update-changelog.sh

--- update-changelog.sh
+++ update-changelog.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-version=$(cat $DIR/package.json | grep "\"version" |awk '{print $2}' | sed 's/[",]//g')
-vim -c "execute \"+normal! O## $version\<cr>\<cr>\<esc>k\""  -c startinsert $DIR/CHANGELOG.md
-git add $DIR/CHANGELOG.md
+version=$(cat package.json | grep "\"version" |awk '{print $2}' | sed 's/[",]//g')
+vim -c "execute \"+normal! O## $version\<cr>\<cr>\<esc>k\""  -c startinsert CHANGELOG.md
+git add CHANGELOG.md

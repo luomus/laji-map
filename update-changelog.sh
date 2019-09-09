cd21d195a3c1b9157e806a3925e0b8a0ae496f41
Olli Raitio <olli.raitio@helsinki.fi>
2019-09-09 13:09:09 +0300

    fix update-changelog.sh

--- update-changelog.sh
+++ update-changelog.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-version=$(cat package.json | grep "\"version" |awk '{print $2}' | sed 's/[",]//g')
+version=$(npm version | head -n 2 | tail -n 1 |awk '{print $2}' | sed "s/[\',]//g")
+echo $version
 vim -c "execute \"+normal! O## $version\<cr>\<cr>\<esc>k\""  -c startinsert CHANGELOG.md
 git add CHANGELOG.md
